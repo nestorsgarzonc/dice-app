b9a2e9592bc3ede71aac74a8bac1776310b97aea
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2019-12-11 20:03:15 -0500

    reactive buttons

--- lib/main.dart
+++ lib/main.dart
@@ -21,17 +21,23 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Image(
-              image: AssetImage('images/dice1.png'),
+            child: FlatButton(
+              onPressed: () {},
+              child: Image(
+                image: AssetImage('images/dice1.png'),
+              ),
             ),
           ),
           Expanded(
-            child: Image(
-              image: AssetImage('images/dice2.png'),
+            child: FlatButton(
+              onPressed: () {},
+              child: Image(
+                image: AssetImage('images/dice2.png'),
+              ),
             ),
           ),
         ],
